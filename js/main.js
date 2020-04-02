ced893d99466d1e78063685df95950b379602c36
IAMNABEEL <39550162+IAMNABEEL@users.noreply.github.com>
2020-04-02 14:22:29 +0500

    Modified CSS

--- js/main.js
+++ js/main.js
@@ -2,6 +2,22 @@
 $(document).ready(function() {
 	('use strict');
 
+	var doAnimations = function() {
+		var offset = $(window).scrollTop() + $(window).height(),
+			$animatables = $('.animatable');
+		if ($animatables.length == 0) {
+			$(window).off('scroll', doAnimations);
+		}
+		$animatables.each(function(i) {
+			var $animatable = $(this);
+			if ($animatable.offset().top + $animatable.height() - 20 < offset) {
+				$animatable.removeClass('animatable').addClass('animated');
+			}
+		});
+	};
+	$(window).on('scroll', doAnimations);
+	$(window).trigger('scroll');
+
 	// ========================================================================= //
 	//  //SMOOTH SCROLL
 	// ========================================================================= //
