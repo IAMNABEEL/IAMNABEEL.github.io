d6a7f05cc204be8eff5bbf1dfac8778c25bf69a7
IAMNABEEL <39550162+IAMNABEEL@users.noreply.github.com>
2020-08-13 19:51:03 +0500

    update main.js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 /*global $, jQuery, alert*/
-$(document).ready(function() {
+$(document).ready(function () {
 	('use strict');
 
-	var doAnimations = function() {
+	var doAnimations = function () {
 		var offset = $(window).scrollTop() + $(window).height(),
 			$animatables = $('.animatable');
 		if ($animatables.length == 0) {
 			$(window).off('scroll', doAnimations);
 		}
-		$animatables.each(function(i) {
+		$animatables.each(function (i) {
 			var $animatable = $(this);
 			if ($animatable.offset().top + $animatable.height() - 20 < offset) {
 				$animatable.removeClass('animatable').addClass('animated');
@@ -24,11 +24,11 @@ $(document).ready(function() {
 
 	$(document).on('scroll', onScroll);
 
-	$('a[href^="#"]').on('click', function(e) {
+	$('a[href^="#"]').on('click', function (e) {
 		e.preventDefault();
 		$(document).off('scroll');
 
-		$('a').each(function() {
+		$('a').each(function () {
 			$(this).removeClass('active');
 			if ($(window).width() < 768) {
 				$('.nav-menu').slideUp();
@@ -43,13 +43,12 @@ $(document).ready(function() {
 		target = $(target);
 		$('html, body')
 			.stop()
-			.animate(
-				{
+			.animate({
 					scrollTop: target.offset().top - 80,
 				},
 				500,
 				'swing',
-				function() {
+				function () {
 					window.location.hash = target.selector;
 					$(document).on('scroll', onScroll);
 				}
@@ -59,7 +58,7 @@ $(document).ready(function() {
 	function onScroll(event) {
 		if ($('.home').length) {
 			var scrollPos = $(document).scrollTop();
-			$('nav ul li a').each(function() {
+			$('nav ul li a').each(function () {
 				var currLink = $(this);
 				var refElement = $(currLink.attr('href'));
 			});
@@ -70,7 +69,7 @@ $(document).ready(function() {
 	//  //NAVBAR SHOW - HIDE
 	// ========================================================================= //
 
-	$(window).scroll(function() {
+	$(window).scroll(function () {
 		var scroll = $(window).scrollTop();
 		if (scroll > 200) {
 			$('#main-nav, #main-nav-subpage').slideDown(700);
@@ -86,7 +85,7 @@ $(document).ready(function() {
 	//  // RESPONSIVE MENU
 	// ========================================================================= //
 
-	$('.responsive').on('click', function(e) {
+	$('.responsive').on('click', function (e) {
 		$('.nav-menu').slideToggle();
 	});
 
@@ -96,7 +95,7 @@ $(document).ready(function() {
 
 	var typed = $('.typed');
 
-	$(function() {
+	$(function () {
 		typed.typed({
 			strings: ['Nabeel Ahmed Rana', 'Dreamer', 'Designer', 'Developer', 'Freelancer', 'Entrepreneur'],
 			// strings: ['Nabeel Ahmed Rana'],
@@ -116,14 +115,24 @@ $(document).ready(function() {
 		dots: true,
 		nav: false,
 		responsiveClass: true,
-		responsive: { 0: { items: 1 }, 768: { items: 2 }, 900: { items: 4 } },
+		responsive: {
+			0: {
+				items: 1
+			},
+			768: {
+				items: 2
+			},
+			900: {
+				items: 4
+			}
+		},
 	});
 
 	// ========================================================================= //
 	//  magnificPopup
 	// ========================================================================= //
 
-	var magnifPopup = function() {
+	var magnifPopup = function () {
 		$('.popup-img').magnificPopup({
 			type: 'image',
 			removalDelay: 300,
@@ -140,7 +149,7 @@ $(document).ready(function() {
 				// The "opener" function should return the element from which popup will be zoomed in
 				// and to which popup will be scaled down
 				// By defailt it looks for an image tag:
-				opener: function(openerElement) {
+				opener: function (openerElement) {
 					// openerElement is the element on which popup was initialized, in this case its <a> tag
 					// you don't need to add "opener" option if this code matches your needs, it's defailt one.
 					return openerElement.is('img') ? openerElement : openerElement.find('img');
@@ -156,16 +165,18 @@ $(document).ready(function() {
 // ========================================================================= //
 //  Porfolio isotope and filter
 // ========================================================================= //
-$(window).load(function() {
+$(window).load(function () {
 	var portfolioIsotope = $('.portfolio-container').isotope({
 		itemSelector: '.portfolio-thumbnail',
 		layoutMode: 'fitRows',
 	});
 
-	$('#portfolio-flters li').on('click', function() {
+	$('#portfolio-flters li').on('click', function () {
 		$('#portfolio-flters li').removeClass('filter-active');
 		$(this).addClass('filter-active');
 
-		portfolioIsotope.isotope({ filter: $(this).data('filter') });
+		portfolioIsotope.isotope({
+			filter: $(this).data('filter')
+		});
 	});
 });
